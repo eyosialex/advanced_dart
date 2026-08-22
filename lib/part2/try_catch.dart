742b3e281002dfac050088c7c14d105b6cad3c0b
eyosialex <Eyosialex27@gmail.com>
2026-08-22 23:55:44 +0300

    add emplementation of try catch exeption

---
+++ lib/part2/try_catch.dart
@@ -0,0 +1,16 @@
+class TryCatch {
+  void t(){
+    try {
+      int age =int.parse("hello world ");
+      print(age);
+    } catch (e,StackTrace) {
+      print("e $StackTrace");
+    }
+  }
+  void checkAge(int age){
+    if (age<18){
+      throw Exception("this is invalid age ");
+      
+    }
+  }
+}
