5afd5c98cdc4e515c0335ea16166ce782f752009
eyosialex <Eyosialex27@gmail.com>
2026-08-30 22:37:21 +0300

    understand the concept of the setter and getter

--- lib/part3/encapsulation.dart
+++ lib/part3/encapsulation.dart
@@ -1,5 +1,15 @@
 class pr{
-  
+String? name;
+int ?age ;
+String get names1{
+  return this.name??" ";
+}
+set names1 (String name1){
+  name=name1;
+}
+String get names2=> this.name??" ";
+set names2(String name)=>this.name=name;
+   
   int _balance=300;
   void display(){
     print('your balance is $_balance');
@@ -7,8 +17,12 @@ class pr{
 }
 
 void main(){
+
   pr p=pr();
+  
+p.names2="efrem";
   // p.balance=400;
+  print("name: ${p.names2}");
   p.display();
 
 
