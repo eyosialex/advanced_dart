cec3cd5e984422d420c0c798cde615a31ef042a2
eyosialex <Eyosialex27@gmail.com>
2026-08-31 05:03:00 +0300

    how to add super()

--- lib/part3/inherite.dart
+++ lib/part3/inherite.dart
@@ -1,17 +1,26 @@
 class animal{
+  String name ;
+  String sound;
+  animal(this.name,this.sound);
+
   void eat(){
     print("all animal can eat");
   }
 
 }
 class dog extends animal{
+  String wolk;
+  dog(this.wolk,String name ,String sound):super(name,sound);
+  void display(){
+    print("name:${this.name} sound:${this.sound} wolk:${this.wolk}");
+  }
   
 }
 
 void main()
 {
-dog d=dog();
-d.eat();
+dog d=dog("run run away","pupy","dady");
+d.display();
 
 
 
